029e88b2bd3c924a8dd265c442c0bac37147f859
Ilya Danilov <ilya.danilov@mirapolis.ru>
2025-12-08 03:49:39 +0500

    init fix

--- ArmenianNews/__init__.py
+++ ArmenianNews/__init__.py
@@ -7,7 +7,10 @@ from .preprocessing import ArmenianTextPreprocessor
 from .triplet_generation import create_all_triplets
 from .models.embedding_model import ArmenianEmbeddingModel
 from .models.ner_model import ArmenianNERModel
-from ArmenianNews.clustering.clustering import NewsClustering
+from .clustering.similarity import SimilarityCalculator
+from .clustering.clustering import NewsClustering
+from .clustering.analysis import ClusterAnalyzer
+from .utils.visualization import NewsVisualization
 
 __version__ = "0.1.1"
 __author__ = "Ilya Danilov"
